981fb88db30b7dea78ba8c746222bfb20536b723
Benjamin Bell <benjamin.bell@tvcs.co.uk>
2017-02-21 15:06:36 -0800

    read write lock test implementation

--- kern/test/rwtest.c
+++ kern/test/rwtest.c
@@ -16,12 +16,124 @@
  * Use these stubs to test your reader-writer locks.
  */
 
+#define NLOOPS 250
+#define NTHREADS 32 
+
+static volatile unsigned long testval1;
+static volatile unsigned long testval2;
+
+static struct rwlock *rwlock = NULL;
+static struct semaphore *exitsem = NULL;
+
+static struct spinlock status_lock;
+static bool test_status = TEST161_FAIL;
+
+static
+bool
+failif(bool condition) {
+	if (condition) {
+		spinlock_acquire(&status_lock);
+		test_status = TEST161_FAIL;
+		spinlock_release(&status_lock);
+	}
+	return condition;
+}
+
+static
+void
+writethread(void *junk1, unsigned long num)
+{
+	(void)junk1;
+
+	unsigned i;
+
+	kprintf_n("Write thread %2lu starting...\n", num);
+	random_yielder(4);
+	for (i=0; i < NLOOPS; i++) {
+		random_yielder(4);
+		rwlock_acquire_write(rwlock);
+		spinlock_acquire(&status_lock);
+		testval1 = 0;
+		testval2 = num;
+		spinlock_release(&status_lock);
+		random_yielder(4);
+		failif(testval1 != 0);
+		random_yielder(4);
+		failif(testval2 != num);
+		rwlock_release_write(rwlock);
+	}
+	kprintf_n("Write thread %2lu ending...\n", num);
+	V(exitsem);
+}
+
+static
+void
+readthread(void *junk1, unsigned long num)
+{
+
+	(void)junk1;
+	unsigned i;
+
+	kprintf_n("Read thread %2lu starting...\n", num);
+	random_yielder(4);
+
+	for (i=0; i < NLOOPS; i++) {
+		random_yielder(4);
+		rwlock_acquire_read(rwlock);
+		spinlock_acquire(&status_lock);
+		testval1++;
+		spinlock_release(&status_lock);
+		random_yielder(4);
+		failif(testval1 == 0);
+		random_yielder(4);
+		rwlock_release_read(rwlock);
+	}
+	kprintf_n("Read thread %2lu ending...\n", num);
+	V(exitsem);
+}
+
 int rwtest(int nargs, char **args) {
 	(void)nargs;
 	(void)args;
 
-	kprintf_n("rwt1 unimplemented\n");
-	success(TEST161_FAIL, SECRET, "rwt1");
+	int i, result;
+
+	kprintf_n("Starting rwt1...\n");
+	exitsem = sem_create("exitsem", 0);
+	if (exitsem == NULL) {
+		panic("rwt1: sem_create failed\n");
+	}
+	rwlock = rwlock_create("rwlock");
+	if (rwlock == NULL) {
+		panic("rwt1: rwlock_create failed");
+	}
+	spinlock_init(&status_lock);
+	test_status = TEST161_SUCCESS;
+	testval1 = 0;
+	
+	for (i=0; i < NTHREADS; i++) {
+		result = thread_fork("rwt1", NULL, writethread, NULL, i);
+		if (result) {
+			panic("rwt1: thread_fork failed\n");
+		}
+		result = thread_fork("rwt1", NULL, readthread, NULL, i);
+		if (result) {
+			panic("rwt1: thread_fork failed\n");
+		}
+	}
+
+	for (i=0; i < NTHREADS; i++) {
+		P(exitsem);
+		P(exitsem);
+	}
+
+	sem_destroy(exitsem);
+	rwlock_destroy(rwlock);
+	exitsem = NULL;
+	rwlock = NULL;
+
+	kprintf_n("\n");
+	success(test_status, SECRET, "rwt1");
 
 	return 0;
 }
